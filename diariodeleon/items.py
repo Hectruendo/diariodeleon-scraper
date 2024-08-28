2a9275cc06cb2d128b3c2f1edaa22839dd7eba65
Hector Soria <hector.soria@with-madrid.com>
2024-08-28 15:59:06 +0200

    structuring the articles

--- diariodeleon/items.py
+++ diariodeleon/items.py
@@ -1,12 +1,36 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
+from scrapy.loader.processors import MapCompose, Join, TakeFirst
+import re
 
 
 class DiariodeleonItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    subtitle = scrapy.Field()
+    category = scrapy.Field()
+    publication_date = scrapy.Field()
+    updated_date = scrapy.Field()
+    author = scrapy.Field()
+    location = scrapy.Field()
+    tags = scrapy.Field()
+    content = scrapy.Field()
+    images = scrapy.Field()
+    image_caption = scrapy.Field()
+    image_author = scrapy.Field()
+    category = scrapy.Field()  # New field for category
+
+
+class DiariodeleonItemLoader(scrapy.loader.ItemLoader):
+    default_output_processor = TakeFirst()
+
+    # Processors for specific fields
+    content_in = MapCompose(str.strip)  # Remove leading/trailing spaces from content
+    content_out = Join('\n')  # Join paragraphs with a newline
+    tags_out = MapCompose(str.strip)  # Strip whitespace from each tag
+    images_out = MapCompose(lambda v: {'url': v.get('url'), 'caption': v.get('caption')})
+
+    # Publication date processor to clean up the date format
+    publication_date_in = MapCompose(lambda x: re.sub(r'\s*\|\s*', ' ', x))
+    updated_date_in = MapCompose(lambda x: re.sub(r'\s*\|\s*', ' ', x))
+
+    def add_content_length(self, body):
+        self.add_value('content_length', len(body))
--- diariodeleon/spiders/diariodeleon.py
+++ diariodeleon/spiders/diariodeleon.py
@@ -1,5 +1,8 @@
 import scrapy
+from scrapy.loader.processors import Join
 from scrapy.spiders import SitemapSpider
+
+from diariodeleon.items import DiariodeleonItem, DiariodeleonItemLoader
 
 
 class DiariodeleonSpider(SitemapSpider):
@@ -12,18 +15,19 @@ class DiariodeleonSpider(SitemapSpider):
     # sitemap_rules = [('/some-regex-pattern/', 'parse_page')]
 
     def parse(self, response):
-        # This is the default callback method for the matched URLs.
-        # You can customize this method to parse the content of the pages.
-        
-        # Example: extract the title of the page
-        title = response.xpath('//title/text()').get()
-        
-        # Example: print the URL and title
-        self.logger.info(f'URL: {response.url}')
-        self.logger.info(f'Title: {title}')
-        
-        # You can return the data as a dictionary or further process it
-        yield {
-            'url': response.url,
-            'title': title
-        }
+        loader = DiariodeleonItemLoader(item=DiariodeleonItem(), response=response)
+
+        loader.add_xpath('title', '//h1[@class="c-detail__title"]/text()')
+        loader.add_xpath('subtitle', '//p[@class="c-detail__subtitle"]/text()')
+        loader.add_xpath('category', '//meta[@property="mrf:tags"]/@content')
+        loader.add_xpath('publication_date', '//p[contains(@class, "c-detail__info__more")][1]/a/time/@datetime')
+        loader.add_xpath('updated_date', '//p[contains(@class, "c-detail__info__more")][2]/time/@datetime')
+        loader.add_xpath('author', '//div[@class="c-detail__author__name"]/a/text()')
+        loader.add_xpath('location', '//span[@class="c-detail__author__location"]/text()')
+        loader.add_xpath('tags', '//ul[@class="c-detail__tags__list"]/li/a/text()')
+        loader.add_xpath('content', '//div[@class="c-detail__body"]//p[@class="paragraph"]/text()')
+        loader.add_xpath('images', '//div[@class="c-detail__media__thumb"]/picture/source[1]/@srcset')
+        loader.add_xpath('image_caption', '//figcaption[@class="c-detail__media__txt"]/p/span[1]/text()')
+        loader.add_xpath('image_author', '//figcaption[@class="c-detail__media__txt"]/p/span[2]/text()')
+
+        yield loader.load_item()
